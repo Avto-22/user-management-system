0b650ee3e8fc0a5c298adb7e8f14c4742a93f223
avto-22 <avtandilkinchikidze@gmail.com>
2023-08-27 02:09:48 +0400

    UMS/change-formChanegDetectopPipe-for-forms-with-array

--- src/app/shared/pipes/change-form-detector.pipe.ts
+++ src/app/shared/pipes/change-form-detector.pipe.ts
@@ -5,12 +5,40 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ChangeFormDetectorPipe implements PipeTransform {
   transform(firstForm: any, secondForm: any): boolean {
-    let isFormChanged: boolean = false;
-    Object.keys(firstForm).forEach((key) => {
-      if (firstForm[key] !== secondForm[key]) {
-        isFormChanged = true;
+    return this.isFormChanged(firstForm, secondForm);
+  }
+
+  private isFormChanged(firstValue: any, secondValue: any): boolean {
+    if (Array.isArray(firstValue)) {
+      return this.compareArray(firstValue, secondValue);
+    }
+
+    if (typeof firstValue === 'object' && typeof secondValue === 'object') {
+      for (const key in firstValue) {
+        if (firstValue.hasOwnProperty(key)) {
+          if (this.isFormChanged(firstValue[key], secondValue[key])) {
+            return true;
+          }
+        }
       }
-    });
-    return isFormChanged;
+    } else if (firstValue !== secondValue) {
+      return true;
+    }
+
+    return false;
+  }
+
+  private compareArray(firstArray: any[], secondArray: any[]): boolean {
+    if (firstArray.length !== secondArray.length) {
+      return true;
+    }
+
+    for (let i = 0; i < firstArray.length; i++) {
+      if (this.isFormChanged(firstArray[i], secondArray[i])) {
+        return true;
+      }
+    }
+
+    return false;
   }
 }
