92411dc49bb850a2ca4ff018aafc98d4af56f51f
avto-22 <avtandilkinchikidze@gmail.com>
2023-08-27 15:36:35 +0400

    UMS/change-isUserActive-directive

--- src/app/shared/directives/is-user-active.directive.ts
+++ src/app/shared/directives/is-user-active.directive.ts
@@ -1,23 +1,35 @@
-import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
+import { Directive, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[isUserActive]',
 })
 export class IsUserActiveDirective {
   @Input('isUserActive') set isUserActive(condition: boolean) {
-    if (condition) {
-      this.renderer.setStyle(
-        this.el.nativeElement,
-        'background-color',
-        'rgba(0,0,0,.4)',
-      );
+    this.isActive = condition;
+    if (this.isActive) {
+      this.backgroundColor = 'rgba(0,0,0,.2)';
+      this.cursor = null;
     } else {
-      this.renderer.removeStyle(this.el.nativeElement, 'background-color');
+      this.backgroundColor = null;
     }
   }
 
-  constructor(
-    private el: ElementRef,
-    private renderer: Renderer2,
-  ) {}
+  isActive: boolean = false;
+
+  @HostBinding('style.backgroundColor') backgroundColor: string | null = null;
+  @HostBinding('style.cursor') cursor: string | null = null;
+
+  constructor() {}
+
+  @HostListener('mouseenter') onMouseEnter() {
+    if (this.isActive) return;
+    this.backgroundColor = 'rgba(0,0,0,.1)';
+    this.cursor = 'pointer';
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    if (this.isActive) return;
+    this.backgroundColor = null;
+    this.cursor = null;
+  }
 }
